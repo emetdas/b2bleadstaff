3d5d9f43c2744e4dbf08d4460cdf05b5034ea7dd
emetdas <emet@b2bleadstaff.com>
2021-01-26 20:55:11 +0600

    login code is running

--- admin/login.php
+++ admin/login.php
@@ -1,12 +1,13 @@
 <?php include 'header.php';?>
 <form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post">
 <div class="input-group">
-<i class="far fa-envelope-open"></i>
+    <i class="far fa-envelope-open"></i>
     <input type="text" placeholder="Enter Your Email">
 </div>
 <div class="input-group">
-<i class="fas fa-key"></i>
+    <i class="fas fa-key"></i>
     <input type="text" placeholder="Enter Your Password">
 </div>
+<input type="submit" value="Login" name="login">
 </form>
 <?php include 'footer.php';?>
