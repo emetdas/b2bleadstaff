05bfb15b90d477414922790ed0bcda8b56eafe2e
emetdas <emet@b2bleadstaff.com>
2021-01-26 20:46:17 +0600

    loging pagee coding is running

--- admin/header.php
+++ admin/header.php
@@ -6,3 +6,21 @@
     <?php include 'link.php';?>
 </head>
 <body>
+<header>
+    <nav>
+        <div class="logo">
+        <a href="index.php">
+            <img src="https://b2bleadstaff.com/wp-content/uploads/2020/12/cropped-logo-removebg-preview.png" alt="website-admin logo"></a>
+        </div>
+        <ul class="nav-item">
+            <li class="nav-items"><a href="#">Home</a></li>
+            <li class="nav-items"><a href="#">Add User</a></li>
+            <li class="nav-items"><a href="#">Add Nav Link</a></li>
+            <li class="nav-items"><a href="#">Logout</a></li>
+            <li class="nav-items">  
+                <img src="https://via.placeholder.com/40x40?text=profile/" alt="profile">
+            </li>
+            </div>
+        </ul>
+    </nav>
+</header>
--- admin/link.php
+++ admin/link.php
@@ -1,3 +1,5 @@
 <title>B2Bleadstaff --Business Lead Provider admin</title>
 <link rel="shortcut icon" href="https://b2bleadstaff.com/wp-content/uploads/2020/12/cropped-logo-removebg-preview.png" type="image/x-icon">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../css/style.css">
