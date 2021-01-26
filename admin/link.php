33b4aa8909e3258baca450bc36144cee1705b1d4
emetdas <emet@b2bleadstaff.com>
2021-01-26 20:53:42 +0600

    coding

--- admin/link.php
+++ admin/link.php
@@ -2,4 +2,5 @@
 <link rel="shortcut icon" href="https://b2bleadstaff.com/wp-content/uploads/2020/12/cropped-logo-removebg-preview.png" type="image/x-icon">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 <link rel="stylesheet" href="../css/style.css">
--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,12 @@
 <?php include 'header.php';?>
-
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" method="post">
+<div class="input-group">
+<i class="far fa-envelope-open"></i>
+    <input type="text" placeholder="Enter Your Email">
+</div>
+<div class="input-group">
+<i class="fas fa-key"></i>
+    <input type="text" placeholder="Enter Your Password">
+</div>
+</form>
 <?php include 'footer.php';?>
