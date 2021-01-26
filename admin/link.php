b8af57da81b650708058a70b29b5d403f215af43
emetdas <emet@b2bleadstaff.com>
2021-01-26 15:34:25 +0600

    css file working

--- admin/link.php
+++ admin/link.php
@@ -1,3 +1,3 @@
+<title>B2Bleadstaff --Business Lead Provider admin</title>
 <link rel="shortcut icon" href="https://b2bleadstaff.com/wp-content/uploads/2020/12/cropped-logo-removebg-preview.png" type="image/x-icon">
 <link rel="stylesheet" href="../css/style.css">
-<title>B2Bleadstaff --Business Lead Provider</title>
