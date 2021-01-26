644c1b2e4c387bdfe0d242de308182b2dd0db611
emetdas <emet@b2bleadstaff.com>
2021-01-26 14:41:19 +0600

    some file create and coding

--- admin/config.php
+++ admin/config.php
@@ -2,7 +2,6 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "";
-$con = mysqli_connect($servername,$username,$password,$database) or die("Databse Not conection");
-
+$database = "b2bleadstaff";
+$con = mysqli_connect($servername,$username,$password,$database) or die("Databse Not conection".mysqli_connect_error());
 ?>
